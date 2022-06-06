6d0d8c4434ec931faa56eab762ddb693a06af90e
Maxim G <golubms2010@yandex.ru>
2022-06-06 17:01:49 +0300

    настроить Webpack3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import { FormValidator } from "./scripts/FormValidator.js";
-import { Card } from "./scripts/Card.js";
-import { Section } from "./scripts/Section.js";
-import { Popup } from "./scripts/Popup.js";
-import { PopupWithImage } from "./scripts/PopupWithImage.js";
-import { UserInfo } from "./scripts/UserInfo.js";
-import { PopupWithForm } from "./scripts/PopupWithForm.js";
+import { FormValidator } from "./components/FormValidator.js";
+import { Card } from "./components/Card.js";
+import { Section } from "./components/Section.js";
+import { Popup } from "./components/Popup.js";
+import { PopupWithImage } from "./components/PopupWithImage.js";
+import { UserInfo } from "./components/UserInfo.js";
+import { PopupWithForm } from "./components/PopupWithForm.js";
 
 
 import {
